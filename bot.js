3adb512c8f66f0c8ad5609f82f3d97a5849bfb42
mbarbattini <mbarbattini@gmail.com>
2021-03-01 18:52:17 -0700

    continued development

--- bot.js
+++ bot.js
@@ -4,9 +4,7 @@
 const { prefix, token } = require('./config.json');
 const fs = require('fs');
 const Discord = require("discord.js");
-
 const client = new Discord.Client();
-
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -19,15 +17,29 @@ for (const file of commandFiles) {
 
 client.once("ready", () => {
     console.log('Ready!');
-    console.log(client.user.bot);
+    // bot is the client.user
+    console.log(client.guilds.cache.find());
+    // client.user.setStatus('Watching over...')
+    //    .then(console.log)
+    //    .catch(console.error);
 });
+
+
 
 // dynamic command handler
 client.on('message', message => {
+    console.log('Message event.');
+
+
+
+
+
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
+
+
 
 	if (!client.commands.has(command)) return;
 
@@ -35,13 +47,15 @@ client.on('message', message => {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('There was an error trying to execute that command!');
 	}
 });
 
+
+
 client.login(token);
 
-// client.user.setAvatar('./avatar.webp');
+
 // client.user.setActivity('Watching over...');
 
 
@@ -69,6 +83,7 @@ tricky = function() {
 client.on("message", msg => {
     let randNumber = Math.floor(Math.random()*100);
     if (msg.channel.id === "790739940702814218" && randNumber < 5 && !msg.author.bot) {
+        console.log('Random MM message called.');
         msg.reply(tricky());    
     }
 });
@@ -77,6 +92,7 @@ client.on("message", msg => {
 client.on("message", msg => {
     const randNumber = Math.floor(Math.random()*100);
     if (msg.channel.id === "529460715598381056" && randNumber < 5 && !msg.author.bot) {
+        console.log('Random MM message called.');
         msg.reply(tricky());    
     }
 });
--- commands/numbergame.js
+++ commands/numbergame.js
@@ -1,5 +1,5 @@
 // NumberGame. Bot creates a random number and the user guesses. 
-// Ranking system based on how close the guess is
+// Promoted to Number Wizard Role if exactly correct
 
 module.exports = {
     name: 'numbergame',
@@ -11,18 +11,24 @@ module.exports = {
             return msg.channel.send(`You didn't include any arguments, ${msg.author}!`)
         }
 
-        // randnumber
-        const randNumber = Math.floor(Math.random() * 100000);
+        // message sender roles
+        const member = msg.member;
+        const role = msg.guild.roles.cache.find(role => role.name === 'Number Wizard');
 
+        // randnumber
+        const randNumber = Math.floor(Math.random() * 1000);
         const difference = Math.abs( args[0] - randNumber);
 
         if (difference === 0) {
-            msg.channel.send('YOU GUESSED CORRECTLY!');
+            msg.channel.send('YOU GUESSED CORRECTLY! \n You have been promoted to "Number Wizard"!');
+            if (!member.roles.cache.some(role => role.name === 'Number Wizard')) {
+                // client.guilds.cache
+            }
         } else if (difference > 0 && difference <= 10) {
             msg.channel.send('You were within single digits... Come on man...');
         } else if (difference > 10 && difference <= 100) {
             msg.channel.send("You were really close!");
-        } else if (difference > 100 && difference <= 1000) {
+        } else if (difference > 100 && difference <= 500) {
             msg.channel.send('You were not that close.');
         } else {
             msg.channel.send("You were really far off... Pathetic...");
